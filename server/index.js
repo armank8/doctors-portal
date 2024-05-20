969c34b276da806f088cf3ea5e43190f739fec05
Arman-Kabir <armankabir8@gmail.com>
2024-05-20 15:51:20 +0600

    nodemon mongodb added

--- server/index.js
+++ server/index.js
@@ -16,8 +16,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqjiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal.tysjeor.mongodb.net/?retryWrites=true&w=majority&appName=doctors-portal`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqjiz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -117,7 +118,7 @@ async function run() {
     });
 
     // stripe set up backend
-    app.post('/create-payment-intent',verifyJWT, async (req, res) => {
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const service = req.body;
       const price = service.price;
       const amount = price * 100;
